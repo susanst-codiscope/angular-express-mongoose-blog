535e2cef24acf3ecd2120d3e77002adaab089c71
timothybone <timothybone@gmail.com>
2012-12-29 00:45:37 -0800

    all CRUD works

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 This is the repository for hobotracks.org.
 
 Hobotracks is a website for travelers. 
+
+TODO:
+
+now the fun part...(hard word still lots of): put together a fun way to display hobo-signs!
+
+Everyauth authorization, fuckin' hate having to make a login for my site, i hate when sites make users do that...It's a pretty lame way to prove someones a human anyway, good thing we'z got betterz wayz
--- routes/api.js
+++ routes/api.js
@@ -12,33 +12,46 @@ var Track = mongoose.model('Track', trackSchema);
 /*
 var woods = new Track({title: 'new hobotrack', text: "it's a goodone"});
 woods.save();  
-*/
+////
 
-var data = [];
-
-Track.find({}, function (err, records) {
-  records.forEach(function (track, i) {
-    data.push({
-      id: i,
-      title: track.title,
-      text: track.text.substr(0, 50) + '...'
+Track.find(function (err, tracks) {
+  tracks.forEach(function (track, i) {
+    
+    console.log(track.text);
     });
-  });
-  console.log(data.length);
-});
+  });*/
 
 
 // GET
 
-exports.tracks = function (req, res) {
-    
-        res.json({
-          tracks: data
-        });
+exports.tracks = function (req, res) {  
+  Track.find({}, function (err, tracks) {
+   // console.log("number of tracks: " + tracks.length );
+    //console.log("the records: " + records);
+    /*records.forEach(function (track, i) {
+      data.push({
+        id: i,
+        title: track.title,
+        text: track.text.substr(0, 50) + '...'
+      });
+    });*/
 
+
+        res.json({
+          tracks: tracks
+        });
+  });
 };
 
 exports.post = function (req, res) {
+  Track.findOne({_id: req.params.id}, function(err,obj) {
+    console.log('returns the post: ' + obj);
+  res.json({
+    post: obj
+    });
+});
+};
+/*
   var id = req.params.id;
   if (id >= 0 && id < data.length) {
     res.json({
@@ -46,38 +59,48 @@ exports.post = function (req, res) {
     });
   } else {
     res.json(false);
-  }
-};
+  }*/
+
 
 // POST
 
 exports.addPost = function (req, res) {
-  data.push(req.body);
+  var newTrack = new Track(req.body);
+  newTrack.save();
+  console.log("post added: " + req.body);
   res.json(req.body);
 };
 
 // PUT
 
-exports.editPost = function (req, res) {
-  var id = req.params.id;
 
-  if (id >= 0 && id < data.length) {
-    data[id] = req.body;
-    res.json(true);
-  } else {
-    res.json(false);
-  }
+
+exports.editPost = function (req, res) {
+  //console.log("edit post: " + req.body.title);
+  Track.findByIdAndUpdate(req.params.id, { 
+    $set: { text: req.body.text, title: req.body.title }}, {upsert:true}, function (err, user) {
+      return res.json(true);
+    }
+  );
 };
 
-// DELETE
-
 exports.deletePost = function (req, res) {
-  var id = req.params.id;
-
+  Track.remove({_id: req.params.id}, function (err) {
+    if (!err) {
+      console.log('no delete post error');
+      res.json(true);
+    }
+    else {
+      console.log('delete post error');
+      res.json(false);
+    }
+  });
+  /*var id = req.params.id;
   if (id >= 0 && id < data.length) {
     data.splice(id, 1);
     res.json(true);
   } else {
     res.json(false);
-  }
+  }*/
 };
+
